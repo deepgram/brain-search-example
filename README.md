4c15e0c4d1d29711a024a5175ebc516b98d0dbf0
greedyuser <wallyduck@yahoo.com>
2017-10-25 11:30:49 -0700

    Added collections and examples

--- README.md
+++ README.md
@@ -22,8 +22,19 @@ brainsearch -u <user_id> -t \<token> search phrase
 
 
 # Try these
+
+#### search some local files
 In the repository are two chapters from public domain audio clips of Alice's Adventures in Wonderland. More great clips cn be found on LibriVox.com. Try the following to quickly get you started!
 
-brainsearch -u <user_id> -t \<token> -f ./testdata/* oh my ears and whiskers
+brainsearch -u <user_id> -t \<token> -f ./testdata/*.mp3 oh my ears and whiskers
 
-brainsearch -u <user_id> -t \<token> -f ./testdata/* off with her head
+brainsearch -u <user_id> -t \<token> -f ./testdata/*.mp3 off with her head
+
+#### search collections of files
+Several link collections are in the testdata directory. Try searching some of Shakespeare's sonnets!
+
+brainsearch -u <user_id> -t \<token> -f ./testdata/shakespeare.links within thine own deep sunken eyes
+
+Or find some wisdom and humor from Twain
+
+brainsearch -u <user_id> -t \<token> -f ./testdata/twain.links Persons attempting to find a motive in this narrative
--- brainsearch/__main__.py
+++ brainsearch/__main__.py
@@ -12,10 +12,34 @@ limitations under the License.
 """
 
 from deepgram import Brain
-import argparse, os, time
+import argparse, os, time, urllib.parse
 from glob import glob
-FILE_TYPES = '.mp3 .3gp .aifc .mp4 .ogg .aif .wav .amr .flac .wmv .mpg .mkv .mp2 .mov .webm .3gpp .m4a .wma .aiff .aac .3ga'.split()
+FILE_TYPES = '.mp3 .3gp .aifc .mp4 .ogg .aif .wav .amr .flac .wmv .mpg .mkv .mp2 .mov .webm .3gpp .m4a .wma .aiff .aac .3ga .links'.split()
 
+
+def loadLink(brainAPI, link, previousAssets):
+  urlPath = urllib.parse.urlsplit(link)[2].strip()
+  basename = os.path.basename(urlPath)
+  if basename in previousAssets:
+    print('Reusing: {}'.format(basename))
+    return previousAssets[basename], basename, True
+  else:
+    print('Loading: {}'.format(basename))
+    # We want to make this async so that we can let brain process while we load more assets
+    assetId = brainAPI.createAssetFromURL(link, async=True, metadata={'filename': basename})['asset_id']
+    return assetId, basename, False
+
+def loadFile(brainAPI, path, previousAssets):
+  basename = os.path.basename(path)
+  if basename in previousAssets:
+    print('Reusing: {}'.format(basename))
+    return previousAssets[basename], basename, True
+  else:
+    print('Loading: {}'.format(basename))
+    with open(path, mode='rb') as data:
+      # We want to make this async so that we can let brain process while we load more assets
+      assetId = brainAPI.uploadAsset(data, async=True, metadata={'filename': basename})['asset_id']
+      return assetId, basename, False
 
 def main():
   parser = argparse.ArgumentParser()
@@ -25,8 +49,10 @@ def main():
   parser.add_argument('-r', '--reload', help='Reload all data files to the server.', action='store_true')
   parser.add_argument('-q', '--quality', help='Minimum quality result to report.', type=float, default=.6)
 
+
   parser.add_argument('query', nargs='+', help='Phrase to search for')
-  parser.add_argument('-f', '--file', help='Files to search in.', action='append')
+  parser.add_argument('-f', '--file', help='Files to search or a link file to get link to search from.', action='append')
+  parser.add_argument('-l', '--link', help='Link to search.', action='append')
   args = parser.parse_args()
 
   #Argparse is great, but unfortunately specifying a default will append it to the list so we will check for it
@@ -39,10 +65,11 @@ def main():
   for file in fileArgs:
     files += [file for file in glob(file) if (os.path.splitext(file)[1].lower() in FILE_TYPES)]
 
-  if len(files) == 0:
-    print('No valid input files found.')
+  if len(files) == 0 and args.link is None:
+    print('No valid inputs found.')
     exit(0)
 
+  #Connect to Brain
   if args.server is not None:
     brainAPI = Brain(url=args.server, user_id= args.user_id, token = args.token)
   else:
@@ -60,16 +87,26 @@ def main():
   assetIds = {}
   loadingAssets = set()
   for file in files:
-    basename = os.path.basename(file)
-    if basename in previousAssets:
-      print('Reusing: {}'.format(basename))
-      assetIds[previousAssets[basename]] = basename
+    if os.path.splitext(file)[1].lower() == '.links':
+      with open(file) as inFile:
+        for link in inFile:
+          assetId, basename, reused = loadLink(brainAPI, link, previousAssets)
+          assetIds[assetId] = basename
+          if not reused:
+            loadingAssets.add(assetId)
+
     else:
-      print('Loading: {}'.format(basename))
-      with open(file, mode='rb') as data:
-        #We want to make this async so that we can let brain process while we load more assets
-        assetId = brainAPI.uploadAsset(data, async=True, metadata={'filename':basename})['asset_id']
-        assetIds[assetId] = basename
+      assetId, basename, reused = loadFile(brainAPI, file, previousAssets)
+      assetIds[assetId] = basename
+      if not reused:
+        loadingAssets.add(assetId)
+
+  #now lets load any urls into brain or re-use them if they are already there
+  if args.link is not None:
+    for link in args.link:
+      assetId, basename, reused = loadLink(brainAPI, link, previousAssets)
+      assetIds[assetId] = basename
+      if not reused:
         loadingAssets.add(assetId)
 
   #Since we loaded asynchronously we need to wait untill all assets have finished loading to search them
